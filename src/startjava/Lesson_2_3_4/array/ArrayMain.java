35192305b201f426e27a1eba73e4f193c573a450
Ivan <vanadiy94@mail.ru>
2020-09-21 13:50:05 +0500

    change ArrayMain.java

--- src/startjava/Lesson_2_3_4/array/ArrayMain.java
+++ src/startjava/Lesson_2_3_4/array/ArrayMain.java
@@ -1,4 +1,36 @@
 package startjava.Lesson_2_3_4.array;
 
 public class ArrayMain {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[6];
+
+        float[] numbers1 = { 5.1f, .5f, 200.5f };
+
+        System.out.println(numbers.length);
+        System.out.println(numbers1.length);
+
+        //System.out.println(numbers[0]);
+        //System.out.println(numbers[5]);
+
+        for (float v : numbers1) {
+            System.out.print(v + " ");
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i*10;
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i]+" ");
+        }
+        //System.out.println(numbers[10]);
+
+        String[] text = new String[3];
+
+        for (String string : text) {
+            System.out.println(string);
+        }
+
+    }
 }
